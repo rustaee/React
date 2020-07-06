bfaa499f7dca40d9c1d8876598bb94cefaf7f4ff
Roustaei <mahdieh.roustaei@gmail.com>
2020-07-06 14:08:10 +0200

    Controlled Forms

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -11,6 +11,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Contact from './ContactComponent';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -47,6 +48,12 @@ class Main extends Component {
       );
     };
 
+    const AboutUs = () => {
+      return(
+        <About leaders={this.state.leaders} />
+      );
+    }
+
     return (
       <div>
         <Header />
@@ -54,6 +61,7 @@ class Main extends Component {
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Route path='/menu/:dishId' component={DishWithId} />
+            <Route exact path='/aboutus' component={AboutUs} />
             <Route exact path='/contactus' component={Contact} />
             <Redirect to="/home" />
         </Switch>
